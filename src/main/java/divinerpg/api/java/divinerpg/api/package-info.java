6cd5335e50f6aed15eefb0a6d9560b706e6e500b
Immortallis1 <shieldfury555@gmail.com>
2019-10-24 13:35:14 -0400

    fresh start

--- src/main/java/divinerpg/api/java/divinerpg/api/package-info.java
+++
@@ -1,5 +0,0 @@
-// TODO find out what 'provides' means
-@API(apiVersion = Reference.API_VERSION, owner = Reference.MODID, provides = "DivineAPI")
-package divinerpg.api.java.divinerpg.api;
-
-import net.minecraftforge.fml.common.API;
