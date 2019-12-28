67b6167d158348df4a733820519b807ef584d5b6
oshi4 <sheglovaoe@gmail.com>
2019-12-29 01:45:30 +0300

    Arcana render fix

--- src/main/java/divinerpg/arcana/client/ArcanaRender.java
+++ src/main/java/divinerpg/arcana/client/ArcanaRender.java
@@ -1,5 +1,6 @@
 package divinerpg.arcana.client;
 
+import com.mojang.blaze3d.platform.GlStateManager;
 import divinerpg.DivineRPG;
 import net.minecraft.client.Minecraft;
 import net.minecraft.util.ResourceLocation;
@@ -7,6 +8,7 @@ import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.client.config.GuiUtils;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod.EventBusSubscriber
@@ -22,14 +24,23 @@ public class ArcanaRender {
         if (e.isCanceled())
             return;
 
+        // Advise from here
+        // https://www.minecraftforge.net/forum/topic/73013-1143-gui-class-replaced-custom-hud-help/
+        // Apparently the TEXT 'layer' provides the best result for﻿ rendering custom things﻿﻿﻿﻿
+        if (e.getType() != RenderGameOverlayEvent.ElementType.TEXT)
+            return;
+
         int y = e.getWindow().getScaledHeight() - DivineRPG.CONFIG.arcanaBar.height.get();
         int x = e.getWindow().getScaledWidth() - DivineRPG.CONFIG.arcanaBar.width.get();
 
-        // TODO arcana renders over heart bar
-        // use transparent background
-//        GL11.glEnable(GL11.GL_BLEND);
-//        mc.getTextureManager().bindTexture(bar);
-//        GuiUtils.drawTexturedModalRect(x, y, 0, 0, 100, 9, 0);
-//        GuiUtils.drawTexturedModalRect(x, y, 0, 9, percentage, 9, 0);
+        GlStateManager.pushMatrix();
+        GlStateManager.enableBlend();
+
+        mc.getTextureManager().bindTexture(bar);
+        GuiUtils.drawTexturedModalRect(x, y, 0, 0, 100, 9, 0);
+        GuiUtils.drawTexturedModalRect(x, y, 0, 9, percentage, 9, 0);
+
+        GlStateManager.disableBlend();
+        GlStateManager.popMatrix();
     }
 }
